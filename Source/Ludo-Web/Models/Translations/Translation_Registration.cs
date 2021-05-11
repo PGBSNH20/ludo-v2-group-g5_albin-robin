2358c558875884127c09c56ae779da1bf39d2131
Albin <almalbin98@gmail.com>
2021-05-11 13:33:42 +0200

    Added MVC project

---
+++ Source/Ludo-Web/Models/Translations/Translation_Registration.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ludo_Web.Models.Translations
+{
+    public record Translation_Registration
+    {
+        [Display(Name = "FirstName", ResourceType = typeof(Resource))]
+        [Required(ErrorMessageResourceType = typeof(Resource), ErrorMessageResourceName = "FirstNameRequired")]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+    }
+}
