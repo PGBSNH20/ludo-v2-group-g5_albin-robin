cc71c0bd55ef32d62d36bab28d86556d226c011c
Albin <almalbin98@gmail.com>
2021-05-14 08:30:19 +0200

    Separated projects

---
+++ Source/Ludo-Web.MVC-Game/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ludo_Web.MVC_Game.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
